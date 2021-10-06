4a1639655c5ec80688e01d56b3fa91b83ec1a308
David Adi Nugroho <davidadi216@gmail.com>
2021-10-06 19:23:37 +0700

    add twitter card

--- src/data/config.js
+++ src/data/config.js
@@ -4,6 +4,8 @@ module.exports = {
     domain: "greatmind.web.id",
     desc: "Mirror dari situs greatmind.id dengan teknologi terbarukan, cepat sekilat petir.",
     email: "hi@greatmind.web.id",
-    ogImage: "https://greatmind.web.id/img/favicon-32x32.png"
+    // ogImage: "https://greatmind.web.id/img/favicon-32x32.png"
+    // ogImage: "https://banners.beyondco.de/Greatmind%20Mirror.png?theme=light&packageManager=&packageName=&pattern=volcanoLamp&style=style_1&description=Mirror+greatmind.id+cepat+sekilat+petir&md=1&showWatermark=0&fontSize=100px&images=light-bulb",
+    ogImage: "https://cdn.statically.io/og/theme=light&font-size=70/%F0%9F%92%A1%EF%B8%8F%20Greatmind%20Mirror.png",
   }
 }
