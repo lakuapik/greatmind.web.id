274e13bf5a23602750c8b61f747416c3d7d21048
David Adi Nugroho <d@davids.id>
2023-09-13 05:53:30 +0700

    update package and add algolia script

---
+++ algolia.py
@@ -0,0 +1,38 @@
+import os
+from sqlite3worker import Sqlite3Worker
+from algoliasearch.search_client import SearchClient
+
+sqlite = Sqlite3Worker('database.sqlite')
+
+client = SearchClient.create(
+  os.environ['ALGOLIA_APP_ID'],
+  os.environ['ALGOLIA_API_KEY'],
+)
+
+index = client.init_index('articles')
+
+articles = sqlite.execute("SELECT * FROM articles ORDER BY id DESC LIMIT 1")
+
+final_articles = []
+for article in articles:
+  final_articles.append({
+    'objectID': article[0],
+    'id': article[0],
+    'cover': article[1],
+    'category': article[2],
+    'title': article[3],
+    'url': article[4],
+    'excerpt': article[5],
+    'author_avatar': article[6],
+    'author_name': article[7],
+    'date': article[8].strftime('%Y-%m-%d %H:%M:%S'),
+    'topic': article[9],
+    'author_desc': article[10],
+    'content': article[11],
+  })
+
+try:
+  index.save_objects(final_articles, {'autoGenerateObjectIDIfNotExist': True})
+  print('success')
+except:
+  print('failed')
